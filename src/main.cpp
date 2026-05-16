9c52a242f537b99c04d88954e4b244cbb1ff8a4e
Heptari <heptari@outlook.com>
2026-05-16 16:15:17 +0800

    feat: change gpio to pwm pulse led. update the dts file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,41 @@
 #include <zephyr/kernel.h>
 
 #include "zephyr/drivers/gpio.h"
+#include "zephyr/drivers/pwm.h"
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+// static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0),
+// gpios);
+static const struct pwm_dt_spec led = PWM_DT_SPEC_GET(DT_ALIAS(led0));
 static const struct gpio_dt_spec button =
     GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
-
-void thread1_dummy(void* dummy) {
-  ARG_UNUSED(dummy);
-
-  while (1) {
-    k_msleep(1);
-  }
-}
-
-K_THREAD_DEFINE(tid, 256, thread1_dummy, NULL, NULL, NULL, 5, 0, 0);
 
 int main() {
   int ret;
 
-  if (!gpio_is_ready_dt(&led) || !gpio_is_ready_dt(&button)) {
+  if (!pwm_is_ready_dt(&led) || !gpio_is_ready_dt(&button)) {
     return 0;
   }
 
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  if (ret < 0) return 0;
+  uint32_t pulse = 0;
+  uint32_t period = led.period;
+  int step = 200000;
 
   ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
   if (ret < 0) return 0;
 
   while (1) {
     int val = gpio_pin_get_dt(&button);
+    // gpio_pin_toggle_dt(&led);
 
-    if (val > 0) {
-      gpio_pin_toggle_dt(&led);
-      k_sleep(K_MSEC(100));
-    } else {
-      gpio_pin_toggle_dt(&led);
-      k_sleep(K_MSEC(1000));
-    }
+    pwm_set_pulse_dt(&led, pulse);
+    pulse += step;
+
+    if (pulse >= period)
+      pulse = period, step = -step;
+    else if (pulse <= 0)
+      pulse = 0, step = -step;
+
+    k_msleep(10);
 
     printk("LED STATE: %d\n", val);
   }
